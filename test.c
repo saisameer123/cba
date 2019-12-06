1995870fa4f0c5dfbb1d67654614acec42f609aa
saisameer123 <48977006+saisameer123@users.noreply.github.com>
2019-12-06 07:51:31 -0800

    Update test.c

--- test.c
+++ test.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
-int main()
-{
-   // printf() displays the string inside quotation
-   printf("Hello, World!");
-   return 0;
+int main() {
+    int n, reversed_n = 0, remainder, original_n;
+    printf("Enter an integer: ");
+    scanf("%d", &n);
+    original_n = n;
+    // reversed integer is stored in reversedN
+    while (n != 0) {
+        remainder = n % 10;
+        reversed_n = reversed_n * 10 + remainder;
+        n /= 10;
+    }
+    // palindrome if orignalN and reversedN are equal
+    if (original_n == reversed_n)
+        printf("%d is a palindrome.", original_n);
+    else
+        printf("%d is not a palindrome.", original_n);
+    return 0;
 }

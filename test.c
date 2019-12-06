a0c35b1ab5fc2323103e590aec211d21ac0a19a5
saisameer123 <48977006+saisameer123@users.noreply.github.com>
2019-12-06 08:15:38 -0800

    Update test.c

--- test.c
+++ test.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 int main() {
-    int n, reversed_n = 0, remainder, original_n;
+    int n, reversed_n = 0, original_n;
     printf("Enter an integer: ");
     scanf("%d", &n);
     original_n = n;
-    // reversed integer is stored in reversedN
+    // reversed integer is stored in reversed_n
     while (n != 0) {
+        int remainder;
         remainder = n % 10;
         reversed_n = reversed_n * 10 + remainder;
         n /= 10;
     }
-    // palindrome if orignalN and reversedN are equal
+    // palindrome if orignal_n and reversed_n are equal
     if (original_n == reversed_n)
         printf("%d is a palindrome.", original_n);
     else
